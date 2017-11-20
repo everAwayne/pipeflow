d76785a025cca24d6bfa73f71ea59fdfc008c031
everAwayne@gmail.com <everAwayne@gmail.com>
2017-11-20 15:45:45 +0800

    support for task confirm

--- tasks.py
+++ tasks.py
@@ -5,13 +5,15 @@ class Task:
 
     _from indicate which input endpoint task come from,
     _to control which output endpoint task will go
+    _confirm_handle is function to confirm
     """
-    __slots__ = ['_from', '_to', '_data']
+    __slots__ = ['_from', '_to', '_data', '_confirm_handle']
 
     def __init__(self, data=None):
         assert isinstance(data, bytes), "task data must be bytes"
         self._from = None
         self._to = None
+        self._confirm_handle = None
         self._data = data
 
     def set_data(self, data):
@@ -35,3 +37,14 @@ class Task:
 
     def get_to(self):
         return self._to
+
+    def set_confirm_handle(self, confirm_handle):
+        self._confirm_handle = confirm_handle
+
+    def get_confirm_handle(self):
+        return self._confirm_handle
+
+    def confirm(self):
+        if self._confirm_handle is not None:
+            self._confirm_handle()
+            self._confirm_handle = None
